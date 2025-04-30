f2bc21c93ffdb1becdd40ce2eed7b08da293e5bc
Fredrik Norgren <fredrik_norgren@hotmail.com>
2025-04-30 16:45:14 +0200

    Removed unnecessary using statements and lines in csproj-file

--- Lexicon Ovn5 SmartBook/Program.cs
+++ Lexicon Ovn5 SmartBook/Program.cs
@@ -1,19 +1,10 @@
-﻿
-
-using Lexicon_Slutuppgift_SmartBook;
-
-namespace Lexicon_Ovn5_SmartBook
+﻿namespace Lexicon_Ovn5_SmartBook
 {
     internal class Program
     {
-        
         static void Main(string[] args)
         {
             LibraryApp.Start();                          
-        }
-
-       
-
-       
+        }         
     }
 }
--- Lexicon Ovn5 SmartBook/Validation.cs
+++ Lexicon Ovn5 SmartBook/Validation.cs
@@ -1,11 +1,4 @@
-﻿using Lexicon_Ovn5_SmartBook;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lexicon_Slutuppgift_SmartBook
+﻿namespace Lexicon_Slutuppgift_SmartBook
 {
     public static class Validation
     {
